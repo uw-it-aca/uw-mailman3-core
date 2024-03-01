4236331900d6c661933d205ef7770e604c7fec69
Stephen Mats Mats <steve@matsmats.com>
2024-03-01 09:42:17 -0800

    add mkdir and chown to init script

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -6,3 +6,5 @@ echo "Pod Initialization Script"
 addgroup -g 1000 -S mailman
 adduser -S mailman -G mailman -H -u 1000
 
+mkdir -p /opt/mailman-web-data/fulltext_index
+chown mailman /opt/mailman-web-data/fulltext_index
